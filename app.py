e64edb4450559d17bdb0d19a933fdc05fa5f9be8
bertrand <ishimweb123@gmail.com>
2025-01-18 00:05:53 +0200

    Working with retrievers

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 import streamlit as st
 
@@ -16,14 +16,14 @@ st.title("Tax Geek Chatbot")
 
 model = ChatOpenAI(model="gpt-4o-mini")
 
-prompt = ChatPromptTemplate(
+prompt = ChatPromptTemplate.from_template(
     """
     Answer the questions based on the provided context first. Provide
     most accurate responses. If you don't know the answer, you can skip
     the question. If you need more information, you can ask for it.
     <context>
     {context}
-    </context>
+    <context>
     Question: {question}
     """
 )
@@ -31,11 +31,11 @@ prompt = ChatPromptTemplate(
 def vector_embedding():
     if "vector_store" not in st.session_state:
         st.session_state.embeddings = OpenAIEmbeddings()
-        st.session_state.loader = PyPDFDirectoryLoader()
+        st.session_state.loader = PyPDFDirectoryLoader("./documentations")
         st.session_state.docs = st.session_state.loader.load()
         st.session_state.splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         st.session_state.split_docs = st.session_state.splitter.split_documents(st.session_state.docs)
-        st.session_state.vector_store = FAISS.from_documents(st.session_state.split_docs, st.session_state.embeddings)
+        st.session_state.chroma_inst = Chroma(embedding_function=st.session_state.embeddings).add_documents(st.session_state.split_docs)
 
 questionPrompt = st.text_input("Ask anything related to tax law....")
 
@@ -43,12 +43,13 @@ if st.button("Document embeddings"):
     vector_embedding()
     st.write("Document embeddings created")
 
-document_chain = create_stuff_documents_chain(model, prompt)
-retriever = st.session_state.vector_store.as_retriever()
-retrieval_chain = create_retrieval_chain(document_chain, retriever)
+
 
 
 if questionPrompt:
+    document_chain = create_stuff_documents_chain(model, prompt)
+    retriever = st.session_state.chroma_inst.as_retriever()
+    retrieval_chain = create_retrieval_chain(document_chain, retriever)
     response = retrieval_chain.invoke({"question": questionPrompt})
     st.write(response["answer"])
 
