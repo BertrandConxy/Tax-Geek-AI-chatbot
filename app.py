ae640e92a1dc5d13955c27aa3c3b6b594f1d2a97
bertrand <ishimweb123@gmail.com>
2025-01-22 22:25:31 +0200

    use latest way

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
+
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 import streamlit as st
 
@@ -26,21 +31,25 @@ def setup_qa_system():
     retriever = vector_store.as_retriever()
 
     llm = ChatOpenAI(model="gpt-4o-mini")
-    qa_chain = RetrievalQA.from_chain_type(retriever=retriever, llm=llm)
 
-    return qa_chain
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    rag_chain = create_retrieval_chain(retriever, combine_docs_chain)
+
+    return rag_chain
 
 
 if __name__ == "__main__":
     st.title("Tax Geek Chatbot")
 
-    qa_chain = setup_qa_system()
+    rag_chain = setup_qa_system()
     question = st.text_input("Ask anything related to tax law....")
     if question:
-        response = qa_chain.invoke(question)
-        st.write(response['result'])
+        response = rag_chain.invoke({"input": question})
+        st.write(response['answer'])
 
-    # with st.expander("References"):
-    #     for i, doc in enumerate(response['context']):
-    #         st.write(doc.page_content)
-    #         st.write("------------------------------------")
+    with st.expander("References"):
+        for i, doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write("------------------------------------")
