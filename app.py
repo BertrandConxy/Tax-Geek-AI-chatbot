776ffed3a97695f53a555403b110baae1c204faf
bertrand <ishimweb123@gmail.com>
2025-01-22 20:52:00 +0200

    implemented RAG

--- app.py
+++ app.py
@@ -1,59 +1,46 @@
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+
 import streamlit as st
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-st.title("Tax Geek Chatbot")
 
 model = ChatOpenAI(model="gpt-4o-mini")
 
-prompt = ChatPromptTemplate.from_template(
-    """
-    Answer the questions based on the provided context first. Provide
-    most accurate responses. If you don't know the answer, you can skip
-    the question. If you need more information, you can ask for it.
-    <context>
-    {context}
-    <context>
-    Question: {question}
-    """
-)
+def setup_qa_system():
+    loader = PyPDFDirectoryLoader('documentations/')
+    docs = loader.load_and_split()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = splitter.split_documents(docs)
 
-def vector_embedding():
-    if "vector_store" not in st.session_state:
-        st.session_state.embeddings = OpenAIEmbeddings()
-        st.session_state.loader = PyPDFDirectoryLoader("./documentations")
-        st.session_state.docs = st.session_state.loader.load()
-        st.session_state.splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        st.session_state.split_docs = st.session_state.splitter.split_documents(st.session_state.docs)
-        st.session_state.chroma_inst = Chroma(embedding_function=st.session_state.embeddings).add_documents(st.session_state.split_docs)
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(chunks, embeddings)
+    retriever = vector_store.as_retriever()
 
-questionPrompt = st.text_input("Ask anything related to tax law....")
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    qa_chain = RetrievalQA.from_chain_type(retriever=retriever, llm=llm)
 
-if st.button("Document embeddings"):
-    vector_embedding()
-    st.write("Document embeddings created")
+    return qa_chain
 
 
+if __name__ == "__main__":
+    st.title("Tax Geek Chatbot")
 
+    qa_chain = setup_qa_system()
+    question = st.text_input("Ask anything related to tax law....")
+    if question:
+        response = qa_chain.invoke(question)
+        st.write(response['result'])
 
-if questionPrompt:
-    document_chain = create_stuff_documents_chain(model, prompt)
-    retriever = st.session_state.chroma_inst.as_retriever()
-    retrieval_chain = create_retrieval_chain(document_chain, retriever)
-    response = retrieval_chain.invoke({"question": questionPrompt})
-    st.write(response["answer"])
-
-    with st.expander("References"):
-        for i, doc in enumerate(response['context']):
-            st.write(doc.page_content)
-            st.write("------------------------------------")
+    # with st.expander("References"):
+    #     for i, doc in enumerate(response['context']):
+    #         st.write(doc.page_content)
+    #         st.write("------------------------------------")
